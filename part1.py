0458db688dbfc0efac154b120c9711de7074a28e
lundjrl <jrlbidamin@gmail.com>
2019-09-28 16:07:37 -0400

    Added initial file

---
+++ part1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#Alphabet = {000, .... 999}
+#L={w|w contains an even number of the two letter sequence 105102 and an odd number of the five letter sequence 119104105108101}
+
+#Output should be a sequence of states that the machine goes through; an output of ACCEPT or REJECT depending on the input string.
+
+class State:
+    def run(self):
+
